d0e571c0046613ce96ce6c67302fc4dee799e137
rayan <rayanhassan200691@gmail.com>
2026-06-07 16:44:47 +0500

    update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Ubuntu, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StarBackground from "@/components/ui/star-background";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const ubuntu = Ubuntu({
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-ubuntu",
   subsets: ["latin"],
 });
 
@@ -29,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ubuntu.variable} ${geistMono.variable} antialiased`}
       >
         <StarBackground />
         {children}
